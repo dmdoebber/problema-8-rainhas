3e091977e463b1bccb773fa456e5d0c73ffd77ae
Daniel M <dmdoebber@inf.ufsm.br>
2018-04-22 03:18:44 -0300

    'mudou nada'

--- problema-8-rainhas/Program.cs
+++ problema-8-rainhas/Program.cs
@@ -4,14 +4,29 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace problema_8_rainhas
 {
     class Program
     {
         static void Main(string[] args)
         {
-            
-            
+            testaColisao();
+            Console.ReadKey();
+        }
+
+        private static int[] vet = new int[8];
+
+        private static bool testaColisao()
+        {
+            for (int i = 0; i < 8; i++)
+                vet[i] = 0;
+            for(int i  = 0; i < 8; i++)
+            {
+                Console.Write(vet[i]);
+            }
+
+            return false;
         }
     }
 }
